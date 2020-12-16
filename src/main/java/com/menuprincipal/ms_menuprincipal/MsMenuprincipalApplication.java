f780dc5d2648d2d38e0c7884a288c4d962d0b5ca
malika <maty.la.gui@gmail.com>
2020-12-16 15:48:38 +0100

    first commit

---
+++ src/main/java/com/menuprincipal/ms_menuprincipal/MsMenuprincipalApplication.java
@@ -0,0 +1,13 @@
+package com.menuprincipal.ms_menuprincipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMenuprincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMenuprincipalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menuprincipal/ms_menuprincipal/MsMenuprincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menuprincipal.ms_menuprincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMenuprincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
